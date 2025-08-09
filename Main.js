4439ede83d9a52686c70c4bdb212c2bb23ba94cc
Akshat kumar yadav <akshatyadav664@gmail.com>
2025-08-09 23:57:34 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,26 @@
+const passwordBox= document.getElementById("password");
+// conditions which the pasword does have.
+const length = 8;
+const upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+const lowerCase = "qwertyuiopasdfghjklzxcvbnm";
+const number = "0123456789";
+const symbol = "~!@#$%^&*()_+=-`\|,./[]{}:<>?";
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+};
+
+function copy(){
+   navigator.clipboard.writeText(passwordBox.value).then(()=>{
+    alert("Password copied to clipboard");
+   });
+}
